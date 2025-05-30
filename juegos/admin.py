b62b15d39c1387dd1e173e7377c50eb53c48048b
programacion-facil <programacionfacil.org@gmail.com>
2025-05-30 19:01:28 +0200

    feat: Configure Django Admin to display models

--- juegos/admin.py
+++ juegos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Juego, Plataforma, Genero
 
-# Register your models here.
+# Registra los modelos para que aparezcan en el panel de administración
+admin.site.register(Juego)
+admin.site.register(Plataforma)
+admin.site.register(Genero)
