f27d2b34b7b5045dd11666fbcf989aabc3ea486a
programacion-facil <programacionfacil.org@gmail.com>
2025-05-31 11:48:15 +0200

    feat: Aesthetic improvements made to the game catalog
    The game catalog's design has been updated to enhance the visual experience. Changes include:
    
    - Replaced the generic placeholder logo on game covers with the actual artwork for each video game, providing a more accurate and appealing visual representation.
    - Implemented general UI adjustments for more intuitive navigation and a polished look.

--- juegos/models.py
+++ juegos/models.py
@@ -14,10 +14,10 @@ class Genero(models.Model):
 
 class Juego(models.Model):
     nombre = models.CharField(max_length=100)
-    precio = models.DecimalField(max_digits=6, decimal_places=2)
     descripcion = models.TextField()
-    generos = models.ManyToManyField(Genero, related_name='juegos')
-    plataformas = models.ManyToManyField(Plataforma, related_name='juegos')
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    generos = models.ManyToManyField(Genero)
+    plataformas = models.ManyToManyField(Plataforma)
 
     def __str__(self):
         return self.nombre
